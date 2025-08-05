00d927a71a67a3ba68bfc4cda7c8f7c66d0ccec8
30000ft <iywr@qq.com>
2025-08-05 17:57:23 +0800

    chore: 版本升级到 v1.2.1 ✨
    🎨 主要更新：
    - 全面美化所有模板主页设计
    - 现代化 UI 和动画效果
    - 响应式设计优化
    - 统一的视觉体验

--- bin/create-app.js
+++ bin/create-app.js
@@ -560,7 +560,7 @@ MIT License
 program
   .name('create-solid-app')
   .description('智能化的 TanStack Solid Start 项目脚手架')
-  .version('1.2.0')
+  .version('1.2.1')
 
 program
   .argument('[project-name]', '项目名称')
