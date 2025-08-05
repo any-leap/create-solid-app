732c0682a74e7be6a585f23d9bbd6e24a10f8601
30000ft <iywr@qq.com>
2025-08-05 17:40:59 +0800

    chore: 版本升级到 1.1.6

--- bin/create-app.js
+++ bin/create-app.js
@@ -560,7 +560,7 @@ MIT License
 program
   .name('create-solid-app')
   .description('智能化的 TanStack Solid Start 项目脚手架')
-  .version('1.1.5')
+  .version('1.1.6')
 
 program
   .argument('[project-name]', '项目名称')
