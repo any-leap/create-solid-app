709387b441cb2d2e09414a7f3328860af53920ed
30000ft <iywr@qq.com>
2025-08-08 02:42:19 +0800

    style: 代码格式化调整

--- templates/full-stack/src/routes/__root.tsx
+++ templates/full-stack/src/routes/__root.tsx
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 import type { JSX } from 'solid-js'
 import { Outlet, createRootRoute, HeadContent, Scripts } from '@tanstack/solid-router'
-import { ErrorComponent,  } from '@tanstack/solid-start'
+import { ErrorComponent, } from '@tanstack/solid-start'
 import { DefaultCatchBoundary } from '../components/DefaultCatchBoundary'
 import { NotFound } from '../components/NotFound'
 import '../styles/app.css'
